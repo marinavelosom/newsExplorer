0afa96b418f6a0ebd024b98cab981440dc5387f4
Marina Melo <engmarinamelo@gmail.com>
2024-08-06 17:59:35 -0300

    Adiciona tela de arquivo selecionado

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,5 +1,6 @@
 export type RootStackParamList = {
   Home: undefined;
+  Article: { title: string; author: string; content: string; url: string; };
   SearchResults: { query: string };
   Login: undefined;
 };
